1759134617f116591dd475579b37e445b2671166
兩足大貓貓 <14369594+M1Screw@users.noreply.github.com>
2021-05-30 14:06:36 -0400

    init commit

---
+++ api.py
@@ -0,0 +1,32 @@
+import socket
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+app.config['PORT'] = '8080'
+app.config['TIMEOUT'] = 1  # In seconds
+app.config['VERSION'] = 1
+
+@app.route('/tcping', methods=['GET'])
+def tcping():
+    request_parameters = request.args
+    ip = request_parameters.get('ip')
+    port = int(request_parameters.get('port'))
+    if ip is None:
+        return jsonify({"status": "error", "message": "Missing ip parameter"})
+    if port is None:
+        return jsonify({"status": "error", "message": "Missing port parameter"})
+    socks = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socks.settimeout(app.config['TIMEOUT'])
+    result = socks.connect_ex((ip, port))
+    socks.close()
+    if result == 0:
+        return jsonify({"status": "true"})
+    else:
+        return jsonify({"status": "false"})
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<p>The resource could not be found.</p>", 404
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=app.config['PORT'], debug=True)
